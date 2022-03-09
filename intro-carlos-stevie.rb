1691166075235adc0cf45910f7c060ce4e9ae4bf
CarRamos <goodsleep415@gmail.com>
2022-03-09 14:58:14 -0800

    ruby intro

---
+++ intro-carlos-stevie.rb
@@ -0,0 +1,16 @@
+puts 1+1
+puts 2*4
+puts 50/10
+puts 4.0/3
+puts 6%4
+puts 10.0/4
+# puts 1/0
+# puts 0/0
+
+my_favorite_number = 420
+puts my_favorite_number
+
+puts my_favorite_number / 2
+someones_favorite = 13
+someones_favorite << 7
+my_favorite_number - someones_favorite
