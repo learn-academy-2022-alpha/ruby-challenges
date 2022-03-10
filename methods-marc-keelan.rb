d6234630cfdee0ab5f3c2d70f5c15edd145be1f6
Marc Matias <mrcallenmatias@gmail.com>
2022-03-10 14:23:15 -0800

    challenges

--- methods-marc-keelan.rb
+++ methods-marc-keelan.rb
@@ -1,4 +1,4 @@
-# Challenges
+
 # Create a method called sum_these_numbers which takes two integers as an argument and prints their sum to the screen.
 
 def sum_these_numbers(num1,num2)
@@ -45,13 +45,9 @@ end
 p isPalindrome("racecar")
 p isPalindrome("dog")
 
-
-
-
 # Challenge: Password Checker
 # User Stories
 # You are writing the user registration page for a secure web site. On the registration page, the user has to enter a user ID and a password, which has to adhere to the to following criteria:
-
 # As a developer, I can create a method that checks for the following rules for a user ID and password:
 # User ID and password cannot be the same.
 # User ID and password must be at least six characters long.
@@ -62,3 +58,30 @@ p isPalindrome("dog")
 # As a user, I can enter my user ID and password into the terminal after being prompted to find out if the they are acceptable.
 # User Stories: Super Stretch
 # As a developer, my method ensures that the user's password must contain at least one number.
+
+p 'Create a username'
+user_name = gets.chomp
+p 'Create a password'
+user_password = gets.chomp
+
+def userChecker (name, password)
+    if (name === password)
+        "Username and password cannot be the same."
+    elsif (name.length < 6 || password.length < 6)
+        "Username and password must be atleast 6 characters long."
+    elsif !password.include?("!") && !password.include?("#") && !password.include?("$") || password.count('0-9') == 0
+        "Password must include !, # or $"
+    elsif (name.include?("!") || name.include?("#") || name.include?("$") || name.include?(" "))
+        "Username must not contain at least !, #, $ or spaces."
+    elsif (password === "password")
+        'Password cannot be the word "password".'
+    else
+        "Username: #{name}\nPassword: #{password}"
+    end
+end
+
+puts userChecker(user_name, user_password)
+
+
+
+
