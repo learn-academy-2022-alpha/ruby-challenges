dfb05d5b725d0ffef372c4b72a9aaad4805ef1ef
tiffanydoh <tiffanydoh@gmail.com>
2022-03-10 16:58:29 -0800

    complete challenge

--- blocks-tiffany-dane.rb
+++ blocks-tiffany-dane.rb
@@ -53,11 +53,69 @@ def largest_num(array)
   array.max
 end
 
-p largest_num([10, 82, 20, 29, 100])
+# p largest_num([10, 82, 20, 29, 100])
 
 # Create a method that takes in an array of numbers and returns the smallest number in the array.
+def smallest_num(array)
+  array.min
+end
+
+p smallest_num([6, 1, 18, 20])
+
+
+
 # Create a method that takes in an array of numbers and returns only the odd numbers sorted from least to greatest.
+numbersss = [51, 29, 508, 8, 93]
+def odd_only(array)
+  odd = array.select do |value|
+    value % 2 != 0
+  end
+  odd.sort
+end
+p odd_only(numbersss)
+
 # Create a method that takes in an array of strings and returns all the strings in reverse casing. All uppercased letters should be returned lowercased and all lowercased letters should be returned uppercased.
+reverse_case = ["HI", "alpha", "DANE", "tiffany"]
+def reverse(array)
+   array.map do |value|
+    if value == value.upcase
+      value.downcase
+    elsif value == value.downcase
+      value.upcase
+    end
+  end
+end
+p reverse(reverse_case)
+
 # Create a method that takes in an array of words and returns all the words that start with a particular letter. Ex) Given the array ['Apple', 'Avocado', 'Banana', 'Mango'] and the letter 'a' the method should return ['Apple', 'Avocado']. With the same method, given the letter 'm' should return ['Mango'].
+fruits = ['Apple', 'Avocado', 'Banana', 'Mango']
+letter = 'a'
+
+def part_letter(array, letters)
+  array.select do |value|
+    if value[0].downcase.include?(letters)
+      value
+    end 
+  end
+end 
+p part_letter(fruits, letter)
+
+
+
 # FIZZBUZZ
 # Write a method that prints the numbers from 1 to 100. For multiples of three print Fizz instead of the number, for multiples of five print Buzz instead of the number, for numbers that are multiples of both three and five print FizzBuzz, for all other numbers print the number.
+range = 1..100
+def fizz_buzz(numbers)
+  numbers.map do |value|
+    if value % 3 == 0 && value % 5 == 0
+      'FizzBuzz'
+    elsif value % 3 == 0 
+      'Fizz'
+    elsif value % 5 == 0 
+      'Buzz'
+    else 
+      value
+    end
+  end
+end 
+p fizz_buzz(range)
