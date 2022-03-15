d1cb4b94f0f32e93c78a5f197eaa8ddb20593e47
Jennice <jennicelodi@gmail.com>
2022-03-15 12:31:34 -0700

    cleaned up story text and added ascii art

--- text-game-michael-ramon-jennice/text-game-michael-ramon-jennice.rb
+++ text-game-michael-ramon-jennice/text-game-michael-ramon-jennice.rb
@@ -1,48 +1,92 @@
 class String
     def print_slow
         self.each_char do |c|
-            sleep 0.05
-        print c
+            sleep 0.01
+            print c
+        end
+        ''
     end
-end
     def print_fast
         self.each_char do |c|
             sleep 0.005
-        print c
+            print c
+        end
+        ''
     end
-end
 end
 
 def game_start 
+
+    puts <<-'RR_title'.print_fast
+
+    .%%%%%...%%..%%..%%%%%...%%..%%..........%%%%%...%%%%%%..%%..%%...%%%%..
+    .%%..%%..%%..%%..%%..%%...%%%%...........%%..%%....%%....%%%.%%..%%.....
+    .%%%%%...%%..%%..%%%%%.....%%............%%%%%.....%%....%%.%%%..%%.%%%.
+    .%%..%%..%%..%%..%%..%%....%%............%%..%%....%%....%%..%%..%%..%%.
+    .%%..%%...%%%%...%%%%%.....%%............%%..%%..%%%%%%..%%..%%...%%%%..
+    ........................................................................
+
+    RR_title
+
     puts 'Enter your name' 
     # asking for users name
+
     name = gets.chomp
 
-    puts 'A suspicious man stands before you with his hands closed together.'
-    puts "Well met #{name}, I see you are a junior developer knight. You have arrived at Ruby Kingdom! Do you see that very large tree in the distance? That is the PostgreSQL tree! You must go there and seek out the Ruby shard bearers to obtain the Ruby Ring and become the Ruby lord should you wish to become the Ruby Lord, seek out the Ruby shard bearers!"
-    puts 'Will you move forward and fulfill your destiny? y/n'
-
+    puts "A suspicious man stands before you with his hands closed together.
+    
+    Suspicious man: 'Well met #{name}, I see you are a Junior Developer Knight. You have arrived at Ruby Kingdom! You must go towards the PostgreSQL tree and seek out the Ruby shard bearers to obtain the Ruby Ring and become the Ruby Lord. 
+    
+    Will you move forward to fulfill your destiny?' y/n".print_fast
     quest = gets.chomp
+
     if quest == "y"
-        puts 'Venture forth, tarnish--- I mean Junior Developer Knight'.print_fast
+        puts 'Venture forth, tarnish--- I mean Junior Developer Knight!'.print_slow
     else 
-        puts 'So your adventure ends here!'.print_fast
-        puts 'YOU PASS OUT!!'
+        puts 'Suspicious man:So your adventure ends here!
+        YOU PASS OUT!! GAME OVERR!!!'.print_slow
         sleep 0.05
         game_start
     end
 end   
+
 game_start
 
-def beau1 
-    puts "Beau dances into the scene. He hits you with the question:".print_fast
+def beau1
+
+
+    puts <<-'beau_art'.print_fast      
+    <((((((\\\
+    /      . }\
+    ;--..--._|}
+(\                 '--/\--'  )
+\\                | '-'  :'|
+\\               . -==- .-|
+\\               \.__.'   \--._
+[\\          __.--|       //  _/'--.
+\ \\       .'-._ ('-----'/ __/      \
+\ \\     /   __>|      | '--.       |
+\ \\   |   \   |     /    /       /
+\ '\ /     \  |     |  _/       /
+\  \       \ |     | /        /
+snd    \  \      \        /
+beau_art
+
+    sleep 0.05
+    puts "You approach the first shardbearer, Beau, who dances into the scene. He hits you with the question:".print_slow
+    sleep 0.05
     puts "How many primitive data types are there?".print_slow
     ans1 = gets.chomp
     if ans1 == '6'|| ans1 == 'six'
-        puts "you deal a blow and then next question".print_fast
+        puts "You deal a blow to Beau
+
+        Beau: 'Awesome! Now next question!'
+
+        ".print_slow
         
     else 
-        puts "Beau hits you GAME OVER!".print_fast
+        puts "Beau: 'Oh no! You got a GAME OVER!!".print_slow
+        sleep 0.05
         game_start
     end
 end
@@ -50,11 +94,15 @@ beau1
 def beau2
     puts 'Beau: "True or False, everything in Ruby is an object"'.print_slow
     ans2 = gets.chomp
-    if ans2.downcase == 'true'
-        puts "Beau: congratulations you passed! But you won’t be so lucky next time. Good luck on the next shard bearer, junior dev knight.".print_fast
+    if ans2.downcase == 'true' || ans2.downcase == 't'
+        puts "Beau: Congratulations you passed! But you won’t be so lucky next time. Good luck on the next shard bearer, Junior Dev Knight.
+        
+        Beau dips from the scene.
+        ".print_slow
         
     else
-        puts "Beau hits you GAME OVER!"
+        puts "Beau: 'oh no! GAME OVER!!".print_slow
+        sleep 0.05
         game_start
     end
 end
@@ -62,47 +110,118 @@ beau2
 
 def charlean1
 
-    puts 'Charlean jumps in: "Charlean arrives in her ship: Aloha Hola!~ Are you ready for this challenge? What does the following print: number = 3/2'.print_fast
+
+    puts <<-'charlean_art'.print_fast
+    ___.---------.___
+    .--'  __.-------.__  `--.
+.-------------------------._      ,' _.--'             `--._ `.
+|   .  |#|#|#|#|#|#|#|O .  | \   ,' .'   \ .-=========-. /   `. `.
+|      |#|#|#|#|#|#|#|O    |_/  / .'      \             /      `. \
+`-------------------------'   / /  `.     \           /     .'  \ \
+\\_\_\            / /   . `.    \         /    .'     \ \
+\\_\_\          / /   //   `.   \       /   .'   .    \ \
+_.-\_\__\____.---| /   //      `   \     /  .'     \\    \ |
+,-'___\      ____---| |   ||    ____.------. .'        ||   | |
+/=/  _______________|| |   `    / [| --- .-. \          ||   | |
+|=(  [____(__)O______|| |        | -|(---(   ) )         ||   | |
+\=\ ____      ____  || |   .    \_[|_--- `-' /          ||   | |
+`-._  / ___ ____ ---| |   ||        `------' `.        ||   | |
+`-//_/_/    `---| \   \\      .'  /     \  `.     //    / |
+//_/_/          \ \   \\   .'   /       \   `.   '    / /
+// / /            \ \   ` .'    /         \    `.     / /
+.-------------------------._  \ \  .'     /           \     `.  / /
+|   .  |#|#|#|#|#|#|#|O    | \  \ `.      /             \      .' /
+|      |#|#|#|#|#|#|#|O    |_/   `. `._  / `-=========-' \  _.' ,'
+`-------------------------'       `.  `--.__         __.--'  .'
+    `--.___ `-------' ___.--'
+           `---------'
+    charlean_art
+
+    puts 'Charlean arrives in her ship and beams down.
+    Charlean: Aloha Hola!~ Are you ready for this challenge? What does the following print: number = 3/2'.print_fast
     ans3 = gets.chomp
     if ans3 == '1'|| ans3.downcase == 'one'
-        puts 'You deal a blow to Charlean. "You pulled a fast one, but can you dodge this?'
+        puts 'You deal a blow to Charlean. 
+        
+        Charlean: "You pulled a fast one, but can you dodge this?'.print_slow
     else
-        puts 'Charlean: Can somebody say something? GAME OVER!!'
+        puts 'Charlean: "Can somebody say something?"
+        
+        GAME OVER!!'.print_slow
+        sleep 0.05
         game_start
     end
 end
 charlean1
 def charlean2
-    puts  'What is missing in the code after “gets” for “LEARN Student\n” in order to print “LEARN Student'
+    puts  'Charlean: "What is missing in the code after “gets” for “LEARN Student\n” in order to print “LEARN Student"?'.print_slow
     ans4 = gets.chomp
     if ans4 == '.chomp'
-        puts 'Charlean, well well arent we a promising Junior Developer Knight'
+        puts 'Charlean: "Well well, arent we a promising Junior Developer Knight." 
+        She hands you her Ruby shard and beams back to her ship.
+        The ship jumps into hyperspace!
+        '.print_slow
     else
-        puts 'Can somebody say something? GAME OVER!!'
+        puts 'Charlean: "Can somebody say something?"
+        
+        GAME OVER!!'.print_slow
+        sleep 0.05
         game_start
     end
 end
 charlean2
 def elyse1
-    puts  'Elyse: An implicit return, which means the value of the last line of a method is automatically returned without using the keyword _____.'
+
+    puts <<-'elyse_art'.print_fast
+C@   =.
+/|\/ |
+\|   | __.__
+ /\  |(  |()|
+ / / _|_) |__|
+elyse_art
+
+    puts 'You hear singing in the distance, the singing gets louder and Elyse teleports before you. 
+    
+    Elyse: "An implicit return, which means the value of the last line of a method is automatically returned without using the keyword _____."'.print_slow
     ans5 = gets.chomp
     if ans5.downcase == 'return'
-        puts 'You deal a blow to elyse and next question!'
+        puts 'You deal a blow to Elyse. Elyse pulls out a microphone
+        
+        Elyse: "Think you can handle this!?~"
+        
+        '.print_slow
     else
-        puts 'Elyse sings “Thats wrong!”, you get hit! GAME OVER!!'
+        puts 'Elyse sings “So loooong~ FAREWELL!~" 
+        
+        GAME OVER!!'.print_slow
+        sleep 0.05
         game_start
     end
 end
 elyse1
 def elyse2
-    puts  'Elyse: Which method would you use to do something with every element of an array and return a new array filled with whatever gets returned by the block each time it runs?'
+    puts  'Elyse: "Which method would you use to do something with every element of an array and return a new array filled with whatever gets returned by the block each time it runs?"'.print_slow
     ans6 = gets.chomp
     if ans6.downcase == 'map do'
-        puts 'you deal a blow to elyse. Elyse is defeated'
+        puts "You deal a blow to Elyse. Elyse drops the mic and is defeated.
+        
+        Elyse: 'Here is your Ruby shard, Good luck, Junior Dev Knight!'
+
+        Elyse exits.
+        ".print_slow
     elsif ans6.downcase == 'map'
-        puts 'close! but not quite! GAME OVER!!'
+        puts 'Elyse: "Close! but not quite!"
+
+        Elyse drops the mic. 
+        
+        GAME OVER!!'.print_slow
+        sleep 0.05
+        game_start
     else
-        puts 'Elyse sings “Thats wrong!”, you get hit! GAME OVER!!'
+        puts 'Elyse sings “So loooong~ FAREWELL!~" 
+        
+        GAME OVER!!'.print_slow
+        sleep 0.05
         game_start
     end
 end
@@ -110,7 +229,7 @@ elyse2
 
 def austin1
 
-    puts <<-'Austin'
+    puts <<-'Austin'.print_fast
                             |\
                              | |
                              | |
@@ -144,169 +263,157 @@ def austin1
                              |/
 Austin
 
-    puts  '# Austin comes in on a surfboard, he kick flips the surfboard and stands his surfboard up. Austin asks “are you a Ruby rockstar? Then answer me this:
-    # “What is the power of using the same methods available to classes like Arrays and Ranges on hashes?”'
+    puts  'Suddenly a tsunami appears. It starts to disippate, as Austin arrives on a surfboard. He kick flips the surfboard asks “I heard you were a Ruby rockstar? “What is the power of using the same methods available to classes like Arrays and Ranges on hashes?”'.print_slow
     ans7 = gets.chomp
     if ans7.downcase == 'duck typing' || ans5.downcase == 'ducktyping'
-        puts 'you deal a blow to Austin and next question!'
+        puts 'You deal a blow to Austin. Austin: "Okay next question!"'.print_slow
     else
-        puts '"But who will fund my island?” You get hit GAME OVER!!'
+        puts '"But who will fund my island?” 
+        
+        GAME OVER!!'.print_slow
+        sleep 0.05
         game_start
     end
 end
 austin1
 
 def austin2
-    puts  'Austin: "What method do you call to initiate a new hash?"'
+    puts  'Austin: "What method do you call to initiate a new hash?"'.print_slow
     ans8 = gets.chomp
     if ans8.downcase == '.new'
-        puts 'Austin:“good job, junior dev knight!”'
+        puts 'Austin:“good job, Junior Dev Knight!” 
+        
+        Austin: "Remember me!"
+        
+        "Remember to help fund my island!"
+        
+        The tsunami reappears and Austin surfs away.
+        '.print_slow
     else
-        puts 'Austin:"But who will fund my island?” You get hit GAME OVER!!'
+        puts '"But who will fund my island?” 
+        
+        GAME OVER!!'.print_slow
+        sleep 0.05
         game_start
     end
 end
 austin2
 
 def sarah1
-    puts  'Sarah:“What does MINASWAN stand for?”'
+    puts  '
+
+In the distance music can be heard, gets louder as it approaches.
+
+"Da-da-da, da-da-da
+Da-da-da, da-da-da"
+
+"Let it never by said that romance is dead
+Cause there’s so little else occupying my head
+There is nothing I need, kept the function to breathe
+But I’m not really fussed, doesn’t matter to me"
+
+"Ruby, ruby, ruby, ruby (ah-ah-ah-ah-ah-ah)"'
+
+puts <<-'sarah_art'.print_fast
+
+__
+/__`.
+/ \ `\\
+/   \  `\
+/     \   \
+/_______\  /\
+(((( ))))
+(((' . ')))
+(((\_-_/)))
+(((_) (_)))
+/((( \ / )))\
+/ (((  ^  ))) \
+/ / ((  ^  )) \ \
+( (   \  ^  /   ) )
+\ \   )www(   / /
+`\\ /     \ //'
+/'       `\
+/           \
+/             \
+/               \
+/                 \
+/                   \
+/                     \
+/                       \
+/                         \
+/                           \ 
+|                             |
+`-----......_____......-----'
+
+sarah_art
+
+puts 'Sarah struts in with her boombox wearing a Ruby crown as she is the current Ruby lord
+
+Sarah: “Hello Junior Developer Knight. You defeated everyone up til now except for me. Do you have what it takes to defeat me and become the Ruby lord?”
+
+Sarah:“What does MINASWAN stand for?”'.print_slow
+
     ans9 = gets.chomp
     if ans9.downcase == 'matz is nice so we are nice'
-        puts 'Sarah:“yes Matz is nice so we are nice, Matz has a very kind demeanor so we treat one another with dignity and respect; we are good to ourselves; and we are good to the codebase itself.“ Sarah takes some damage.'
+        puts 'Sarah:“Yes Matz is nice so we are nice, Matz has a very kind demeanor so we treat one another with dignity and respect; we are good to ourselves; and we are good to the codebase itself.“ Sarah takes a hit
+        
+        She turns up the volume.
+        .'.print_slow
     else
-        puts 'Sarah: “You are not ready to be the Ruby Lord” Blasts "ruby ruby ruby!~" GAME OVER!!'
+        puts 'Sarah:“You are not ready to be the Ruby Lord” 
+        
+        Sarah turns up the volume for "RUBY RUBY RUBY!~" 
+        
+        GAME OVER!!'.print_slow
+        sleep 0.05
         game_start
     end
 end
 sarah1
 
 def sarah2
-    puts  'Sarah: What is the datatype for the “key” in a key: value pair?'
+    puts  'Sarah: What is the datatype for the “key” in a key: value pair?'.print_slow
     ans10 = gets.chomp
     if ans10.downcase == 'symbol'
-        puts 'Sarah:“good job, junior dev knight”'
+        puts 'Sarah:“good job, Junior Dev Knight”'.print_slow
     else
-        puts 'Sarah:“You are not ready to be the Ruby Lord” Sarah turns up the volume for "ruby ruby ruby!~" GAME OVER!!'
+        puts 'Sarah:“You are not ready to be the Ruby Lord” 
+        
+        Sarah turns up the volume for "RUBY RUBY RUBY!~" 
+        
+        GAME OVER!!'.print_slow
+        sleep 0.05
         game_start
     end
 end
 sarah2
 
 def sarah3
-    puts  'Sarah: What year is Ruby invented?'
+    puts  'Sarah: What year is Ruby invented?'.print_slow
     ans11 = gets.chomp
     if ans11 == '1995'
-        puts 'Sarah:“good job, junior dev knight”'
+        puts "Sarah:You dont need to really know that one but good job, junior dev knight".print_slow
     else
-        puts 'Sarah:“you are not ready to be the Ruby Lord” Blasts "ruby ruby ruby!~" GAME OVER!!'
+        puts 'Sarah:“You are not ready to be the Ruby Lord” 
+        
+        Sarah turns up the volume for "RUBY RUBY RUBY!~" 
+        
+        GAME OVER!!'.print_slow
+        sleep 0.05
         game_start
     end
 end
 sarah3
 
 def game_win
-    p 'Sarah passes the boombox and the Ruby crown to you.'
-    p 'SaraH: "You have become the new Ruby Lord."'
-    p 'You walk towards the glowing PostgreSQL tree and the boombox blares as you take your place on the Ruby throne.'
-    p 'Ruby, ruby, ruby, ruby (ah-ah-ah-ah-ah-ah)'
-    p 'do ya, do ya, do ya (ah-ah-ah-ah-ah-ah)'
-    p 'Know what youre doing, doing to me? (ah-ah-ah-ah-ah-ah)'
-    p 'Ruby, ruby, ruby, ruby (ah-ah-ah-ah-ah-ah)'
-    p 'YOU WIN'
+    p 'Sarah passes the boombox and the Ruby crown to you.'.print_fast
+    p 'SaraH: "You have become the new Ruby Lord."'.print_fast
+    p 'You walk towards the glowing PostgreSQL tree and the boombox blares as you take your place on the Ruby throne.'.print_fast
+    p 'Ruby, ruby, ruby, ruby (ah-ah-ah-ah-ah-ah)'.print_fast
+    p 'do ya, do ya, do ya (ah-ah-ah-ah-ah-ah)'.print_fast
+    p 'Know what youre doing, doing to me? (ah-ah-ah-ah-ah-ah)'.print_fast
+    p 'Ruby, ruby, ruby, ruby (ah-ah-ah-ah-ah-ah)'.print_fast
+    p 'YOU WIN'.print_slow
 end
 
 game_win
-
-
-#     Elyse
-# *you hear singing in the distance, the singing gets louder and Elyse teleports before you*
-
-# “Hello and answer me this Junior dev knight”
-
-# 
-
-# map do: deal a blow to elyse
-# Anything else, “Elyse sings “Nooooo!~”
-
-# Elyse congratulates you and sings on her way out
-
-# Austin
-
-
-# 
-# Duck typing: “right on!” Deal a blow to Austin
-# Anything else: “But who will fund my island?” You get hit
-
-# “What method do you call to initiate a new hash?”
-# .new: “good job, junior dev knight!”
-# Anything else: “But who will fund my island?” You get hit
-
-# “.each method can take on two possible arguments, what are they?”
-
-# Key and value: “right on!” Deal a blow to Austin
-# Anything else: “Ooooh so close, that’s less money for my island” You get hit
-
-# When you beat Austin, I look forward to seeing you again. Remember me…
-# remember to help fund my island!
-
-# Austin surfs away
-
-# In the distance music can be heard, gets louder as it approaches.
-
-# Da-da-da, da-da-da
-# Da-da-da, da-da-da
-
-# Let it never by said that romance is dead
-# Cause there’s so little else occupying my head
-# There is nothing I need, kept the function to breathe
-# But I’m not really fussed, doesn’t matter to me
-
-# Ruby, ruby, ruby, ruby (ah-ah-ah-ah-ah-ah)
-# Sarah struts in with her boombox wearing a Ruby crown as she is the current Ruby lord
-
-# “Hello young junior developer knight. You defeated everyone up til now except for me. Do you have what it takes to defeat me and become the Ruby lord?”
-
-# “What does MINASWAN stand for?” 
-# Matz is nice so we are nice: “yes Matz is nice so we are nice, Matz has a very kind demeanor so we treat one another with dignity and respect; we are good to ourselves; and we are good to the codebase itself.“ Sarah takes some damage. 
-# Anything else “you are not ready to be the Ruby Lord”
-# Blasts *ruby ruby ruby*
-
-# “What is the datatype for the “key” in a key: value pair?”
-
-# “Symbol: “good job, junior dev knight”
-# Anything else “you are not ready to be the Ruby Lord”
-# Blasts *ruby ruby ruby*
-
-# What language is ruby-on-rails written in?
-# Ruby: "very good, junior dev knight. It is an honor to be bested by the new Ruby Lord."
-
-# Sarah passes the boombox and the Ruby crown to you.
-
-# You become the new Ruby Lord.
-
-# You walk towards the glowing PostgreSQL tree and the boombox blares as you take your place on the Ruby throne.
-
-# Ruby, ruby, ruby, ruby (ah-ah-ah-ah-ah-ah)
-# do ya, do ya, do ya (ah-ah-ah-ah-ah-ah)
-# Know what you're doing, doing to me? (ah-ah-ah-ah-ah-ah)
-# Ruby, ruby, ruby, ruby (ah-ah-ah-ah-ah-ah)
-
-# YOU WIN
-
-# Stretch Ideas:
-# You can fight the teachers in any order
-# Secret boss would be the founder of LEARN
-# HP system?
-# Randomize certain questions
-# You can heal yourself by drinking a flask but it asks you a react question
-# Checkpoint system?
-
-# Text based game presentations:
-# Text-Based Story Demo (3-5 min)
-# Show us your game and run through a couple scenarios.
-# Person 1
-# - What did your team’s planning process look like?
-# Person 2
-# - Show us one built in Ruby method and explain what it is doing.
-# Person 3
-# - What was something that your team got stuck on and how did you overcome this challenge?
